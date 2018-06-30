6fa98039e38deab3058f62e89f048a95a3b6b249
Nayalash <38357017+Nayalash@users.noreply.github.com>
2018-06-30 17:27:22 -0400

    Add files via upload

---
+++ Counter_BLINK.ino
@@ -0,0 +1,21 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+const int otherLedPin = 12;
+int buttonState;
+void setup() {
+
+  pinMode(ledPin, OUTPUT) ;
+  pinMode(otherLedPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop ()  {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(otherLedPin, LOW);
+  } else {
+    digitalWrite(ledPin,LOW);
+    digitalWrite(otherLedPin, HIGH);
+  }
+}
